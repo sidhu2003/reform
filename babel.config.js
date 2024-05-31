68092855343e170aaab9ba332c57466ea915c77e
sandeeptottadi <sandeeptottadi@gmail.com>
2024-05-31 10:36:13 +0530

    new commit

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,5 @@
 module.exports = {
-  // other Babel options
+  presets: ["@babel/preset-env", "@babel/preset-react"],
+  plugins: ["@babel/plugin-transform-react-jsx"],
   ignore: ["**/*.svg"],
 };
